ac174a3eddfa0d2a20ce8b2c24843ff2d5044d3c
Ludovic LAFFINEUR <ludovic.laffineur@gmail.com>
2015-03-17 13:22:41 +0100

    Initial Commit

---
+++ AudioLR/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  AudioLR
+//
+//  Created by Ludovic Laffineur on 17/03/15.
+//  Copyright (c) 2015 Ludovic Laffineur. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
